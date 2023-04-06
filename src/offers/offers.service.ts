2449863eae90dc7c4df143f7e979f98fbf340081
Victoria Levine <vkpanchenko@gmail.com>
2023-04-06 21:27:53 +0200

    base CRUD ops for entities

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { UpdateOfferDto } from './dto/update-offer.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Offer } from './entities/offer.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class OffersService {}
+export class OffersService {
+  constructor(
+    private dataSource: DataSource,
+    @InjectRepository(Offer)
+    private offerRepository: Repository<Offer>,
+    @InjectRepository(Wish)
+    private wishRepository: Repository<Wish>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(createOfferDto: CreateOfferDto) {
+    return 'This action adds a new offer';
+  }
+
+  findAll() {
+    return `This action returns all offers`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} offer`;
+  }
+
+  updateOne(id: number, updateOfferDto: UpdateOfferDto) {
+    return `This action updates a #${id} offer`;
+  }
+
+  removeOne(id: number) {
+    return `This action removes a #${id} offer`;
+  }
+}
