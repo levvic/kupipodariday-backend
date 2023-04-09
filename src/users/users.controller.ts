88c5ae89afc799bbe430bf3bd5c05a7a2f6c4071
Victoria Levine <vkpanchenko@gmail.com>
2023-04-09 23:03:04 +0200

    feat: users/me endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common/exceptions';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  async getUser(@Req() { user }: { user: User }): Promise<User> {
+    const userData = await this.usersService.findById(user.id);
+
+    if (!userData) {
+      throw new NotFoundException();
+    }
+    const { password, ...result } = userData;
+    return userData;
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('me')
+  async updateUser(
+    @Req() { user }: { user: User },
+    @Body() dto: UpdateUserDto,
+  ): Promise<User> {
+    return await this.usersService.updateById(user.id, dto);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,12 +4,14 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { HashService } from 'src/hash/hash.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private readonly hashService: HashService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -28,6 +30,30 @@ export class UsersService {
 
   async findByEmail(email: string): Promise<User> {
     return await this.usersRepository.findOneBy({ email });
+  }
+
+  async findById(id: number): Promise<User> {
+    return await this.usersRepository.findOneBy({ id });
+  }
+
+  async updateById(id: number, dto: UpdateUserDto): Promise<User> {
+    const user = await this.findById(id);
+    //hash password
+    if (dto.password) {
+      dto.password = await this.hashService.getHashAsync(dto.password);
+    }
+
+    const updatedUser: User = {
+      ...user,
+      password: dto?.password,
+      email: dto?.email,
+      about: dto?.about,
+      username: dto?.username,
+      avatar: dto?.avatar,
+    };
+    await this.usersRepository.update(user.id, updatedUser);
+
+    return await this.findById(id);
   }
 
   updateOne(id: number, updateUserDto: UpdateUserDto) {
